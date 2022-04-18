ddc2d494535d3d297c2db34037130e838fa6afff
Ranga Munasinghe <rangamunasinghe94@gmail.com>
2022-04-18 09:06:42 -0400

    added divs

---
+++ rangle/src/components/GuessPanes.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="main">
+     <div class="tile-row">
+         
+     </div>
+
+     <div class="tile-row">
+
+     </div>
+
+     <div class="tile-row"></div>
+
+     <div class="tile-row"></div>
+
+     <div class="tile-row"></div>
+
+     <div class="tile-row"></div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            
+            currentGuess: {
+                firstLetter : '',
+                secondLetter : '',
+                thirdLetter : '',
+                fourthLetter: '',
+                fifthLetter : ''
+            }
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
